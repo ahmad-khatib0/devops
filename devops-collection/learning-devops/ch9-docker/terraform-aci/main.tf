765e0a6dbde2be50aeb58f46a2af1b290b75cb19
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-11-11 03:40:13 +0300

    docker pipeline

---
+++ devops-collection/learning-devops/ch9-docker/terraform-aci/main.tf
@@ -0,0 +1,40 @@
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "acidemobook" {
+  name     = "demoBook"
+  location = "westus2"
+}
+
+variable "imageversion" {
+  description = "Tag of the image to deploy"
+  default     = "v1"
+}
+
+variable "dockerhub-username" {
+  description = "DockerHub username"
+}
+
+
+resource "azurerm_container_group" "aci-myapp" {
+  name                = "aci-app"
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.acidemobook.name
+  ip_address_type     = "public"
+  dns_name_label      = "myapp-demo"
+  os_type             = "linux"
+
+  container {
+    name   = "myappdemo"
+    image  = "docker.io/${var.dockerhub-username}/demobook:${var.imageversion}"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
---
+++ devops-collection/learning-devops/ch9-docker/terraform-aci/versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.63.0"
+    }
+  }
+  required_version = ">= 0.14"
+}
+
